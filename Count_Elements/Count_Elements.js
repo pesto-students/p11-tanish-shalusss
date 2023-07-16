290b208af4a21cdf9a9df36f2c1d8fb6b9a4d2e0
shalusss <67733785+shalusss@users.noreply.github.com>
2023-07-17 01:58:37 +0530

    Count elements array assignment added

---
+++ Count_Elements/Count_Elements.js
@@ -0,0 +1,7 @@
+function countElements(array) {
+    return array.length;
+}
+
+console.log(countElements([1, 2, 3, 4, 5])); // Test Case 1
+console.log(countElements([7, 8, 9, 10, 11, 12, 13, 14, 15])); // Test Case 2
+
